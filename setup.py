0ecd5d5f5a96acfe4d2964ce7947a420718fa2a1
Anthony Jourdan <terman37@yahoo.fr>
2021-02-22 17:06:12 +0100

    project init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="RL_Maze_Solver",
+    version="0.1.0",
+    author="AJO",
+    packages=find_packages(),
+    python_requires='>=3.6',
+    tests_require=['pytest']
+)
